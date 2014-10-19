dfa3e1b79eb299a0b9e9bf8fac83cd671925c02f
Chris Claydon <cclaydon@hotmail.com>
2014-10-19 21:30:20 +1000

    Add Card Class

---
+++ MercilessDungeon/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MercilessDungeon
+{
+   class Card
+    {
+       // Card properties
+       public string Title { get; private set; }
+       public string Description { get; private set; }
+
+       // Play method
+       public void Play()
+       {
+
+           //Things that happen when a card wants to be played
+       }
+
+       // Constructor
+       public Card(string title, string description)
+       {
+           if (String.IsNullOrWhiteSpace(title)) throw new ArgumentOutOfRangeException("Card title can not be empty");
+           Title = title;
+           if (String.IsNullOrWhiteSpace(description)) throw new ArgumentOutOfRangeException("Card description can not be empty");
+           Description = description;
+
+       }
+
+    }
+}
